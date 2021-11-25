170a8c9bd13beff8ef83d3156743b0b2bdddf255
Milind <milindanand74@gmail.com>
2021-11-25 20:01:13 +0530

    array of graph created

--- script/script.js
+++ script/script.js
@@ -25,3 +25,33 @@ const togglePlay = () => {
   
 //Event listener for Play / Pause audio
 playBtn.addEventListener("click", togglePlay);
+
+
+
+  //function for random value between range
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+  //Array contains bar graph position values
+let graphData = [];
+
+function graph() {
+  for (let i = 1; i < 350; i += 3) {
+    graphData.push({
+      colour: "#fff",
+      width: 1,
+      height: randomNumber(20, 70),
+      top: randomNumber(30, 70),
+      left: i,
+    });
+  }
+}
+
+graph()
+
+//Creating context in the canvas by bar graph data
+graphData.forEach((el) => {
+  context.fillStyle = el.colour;
+  context.fillRect(el.left, el.top, el.width, el.height);
+});
